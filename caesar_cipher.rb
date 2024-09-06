e3538082888443b6a81539a9e73c5b2da3b76662
Volodymyr Pivtoranis <pivtoranis87@gmail.com>
2024-09-06 10:54:14 +0300

    Add caesar cipher project solution

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,1 +1,19 @@
+# frozen_string_literal: true
 
+def caesar_cipher(text, shift)
+  text.chars.map do |char|
+    if char.between?('A', 'Z')
+      # Shift and wrap for uppercase letters
+      ((char.ord - 'A'.ord + shift) % 26 + 'A'.ord).chr
+    elsif char.between?('a', 'z')
+      # Shift and wrap for lowercase letters
+      ((char.ord - 'a'.ord + shift) % 26 + 'a'.ord).chr
+    else
+      # Non-alphabetic characters are not changed
+      char
+    end
+  end.join
+end
+
+p caesar_cipher('Bmfy f xywnsl!', -5)
+p caesar_cipher('!!!Hello!!!', 15)
